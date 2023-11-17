e923af2cb1452cfc81b02a55eb3d8fcf75160dd5
Marius Rundereim <marius.rundereim@gmail.com>
2023-11-17 19:00:16 +0100

    created register page

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -2,25 +2,43 @@ export function header() {
   const htmlHeader = document.createElement("header");
   htmlHeader.innerHTML = `
   <header class="p-2 shadow-lg">
-      <div
-        class="container mx-auto mx-auto flex items-center justify-between space-x-4"
+  <div
+    class="container mx-auto mx-auto flex items-center justify-between space-x-4"
+  >
+    <a href="#" class="text-2xl font-bold font-body">Biddl.</a>
+    <nav class="flex flex-col sm:flex-row gap-2">
+      <ul class="flex flex-col sm:flex-row gap-2">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Listings</a></li>
+        <li><a href="#">Profile</a></li>
+      </ul>
+    </nav>
+
+    <div>
+      <button
+        type="button"
+        class="bg-brand-light p-2 px-4 rounded-2xl hover:bg-brand-color"
+        id="login_btn"
       >
-        <a href="#" class="text-2xl font-bold font-body">Biddl.</a>
-        <nav class="flex flex-col sm:flex-row gap-2">
-          <ul class="flex flex-col sm:flex-row gap-2">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Listings</a></li>
-            <li><a href="#">Profile</a></li>
-          </ul>
-        </nav>
-        <button
-          type="button"
-          class="bg-gray-200 p-2 px-4 rounded-2xl hover:bg-red-400"
-        >
-          Login
-        </button>
-      </div>
-    </header>
+        Login
+      </button>
+      <button
+        type="button"
+        class="bg-brand-light p-2 px-4 rounded-2xl hover:bg-brand-color"
+        id="register_btn"
+      >
+        Register
+      </button>
+      <button
+        type="button"
+        class="text-white bg-brand-dark p-2 px-4 rounded-2xl"
+        id="logout_btn"
+      >
+        Logout
+      </button>
+    </div>
+  </div>
+</header>
   `;
   return htmlHeader;
 }
---
+++ src/js/utils/domElements.mjs
@@ -0,0 +1,5 @@
+// Auth
+export const login_btn = document.querySelector("#login_btn");
+export const logout_btn = document.querySelector("#logout_btn");
+export const email_input = document.querySelector("#registerEmail");
+export const pw_input = document.querySelector("#registerEmail");
