4e0647b789da392fcf766ea812abf1d94fb071ee
Marius Rundereim <marius.rundereim@gmail.com>
2023-11-20 20:18:32 +0100

    fetch profile

---
+++ src/js/profile/profile.mjs
@@ -0,0 +1,30 @@
+import { api_profiles } from "../api/env/env.mjs";
+import { getSearchParams } from "../api/searchParams.mjs";
+import { headers } from "../auth/headers.mjs";
+
+export async function getProfile() {
+  try {
+    const getProfileData = {
+      method: "GET",
+      headers: headers("application/json"),
+      body: JSON.stringify(),
+    };
+
+    const name = getSearchParams().name;
+
+    const response = await fetch(
+      `${api_profiles}/${name}?_listings=true`,
+      getProfileData
+    );
+
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+      renderProfile(result);
+      //viewProfile(result);
+    } else {
+      alert("Please log in");
+    }
+  } catch (error) {}
+}
+getProfile();
