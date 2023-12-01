97e021a018c2445c115297c3cdb9e383d58cdfde
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-01 10:11:37 +0100

    multiple media on listings

--- src/js/listeners/listings/createListing.mjs
+++ src/js/listeners/listings/createListing.mjs
@@ -1,28 +1,30 @@
 import { createListing } from "../../api/listings/createListing.mjs";
 
-// export const listingForm = document
-//   .querySelector("#newListing-form")
-//   .addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const form = e.target;
-//     const newListing = {
-//       title: form.newList_title.value,
-//       description: form.newList_desc.value,
-//       media: Array.of(form.newList_media.value.toString()),
-//       endsAt: new Date(form.newList_endsAt.value.toString()),
-//     };
-//     console.log(newListing);
-//     createListing(newListing);
-//   });
-
 export const listingForm = document
   .querySelector("#newListing-form")
   .addEventListener("submit", (e) => {
     e.preventDefault();
     const form = e.target;
-
     const formData = new FormData(form);
-    const mediaUrls = formData.getAll("media[]");
+
+    const mediaUrls = [];
+    const firstMedia = formData.get("media[]");
+    if (firstMedia) {
+      mediaUrls.push(firstMedia);
+    }
+    const secondMedia = formData.get("media[]");
+    if (secondMedia) {
+      mediaUrls.push(secondMedia);
+    }
+    const thirdMedia = formData.get("media[]");
+    if (thirdMedia) {
+      mediaUrls.push(thirdMedia);
+    }
+    const fourthMedia = formData.get("media[]");
+    if (fourthMedia) {
+      mediaUrls.push(fourthMedia);
+    }
+
     const newListing = {
       title: formData.get("title"),
       description: formData.get("description"),
--- src/js/utils/formatting/formatEndsAt.mjs
+++ src/js/utils/formatting/formatEndsAt.mjs
@@ -20,27 +20,3 @@ export function formatEndsAt(endsAt) {
   // console.log(timeRemaining);
   return timeRemaining;
 }
-
-// const endDate = new Date("2023-12-01T00:00:00.000Z").getTime();
-// // const endDate = new Date().getTime();
-// export function updateRemainingTime() {
-//   const now = new Date().getTime();
-//   const distance = endDate - now;
-
-//   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor(
-//     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-//   );
-//   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//   //   console.log(
-//   //     `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds}`
-//   //   );
-
-//   const timeRemaining = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-//   // console.log(timeRemaining);
-
-//   return timeRemaining;
-// }
-// updateRemainingTime();
-// setInterval(updateRemainingTime, 1000);
