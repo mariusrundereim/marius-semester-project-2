6939caf43b6e61c3f190b824409e04c03768e7d5
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-04 15:05:38 +0100

    improvements on image gallery

--- src/js/ui/listings/listingGallery.mjs
+++ src/js/ui/listings/listingGallery.mjs
@@ -11,66 +11,40 @@ export function imageGallery(listing) {
   );
   gallerySection.appendChild(coverImage); // Create a container for the rest of the media items
 
-  const thumbnailContainer = document.createElement("div");
-  thumbnailContainer.classList.add(
-    "py-2",
-    "flex",
-    "flex-wrap",
-    "gap-1",
-    "justify-center",
-    "items-center"
-  );
-  gallerySection.appendChild(thumbnailContainer);
+  if (listing.media.length > 1) {
+    const thumbnailContainer = document.createElement("div");
+    thumbnailContainer.classList.add(
+      "py-2",
+      "flex",
+      "flex-wrap",
+      "gap-1",
+      "justify-center",
+      "items-center"
+    );
+    gallerySection.appendChild(thumbnailContainer);
 
-  listing.media.slice(0).forEach((mediaUrl, index) => {
-    const thumbnailImage = createImageElement(
-      mediaUrl,
-      "h-10 md:h-20 aspect-square object-cover rounded-md brightness-50"
-    );
-    thumbnailImage.addEventListener("click", () =>
-      handleThumbnailClick(index, listing.media)
-    );
-    thumbnailContainer.appendChild(thumbnailImage);
-  });
+    listing.media.slice(0).forEach((mediaUrl, index) => {
+      const thumbnailImage = createImageElement(
+        mediaUrl,
+        "h-10 md:h-20 aspect-square object-cover rounded-md brightness-50"
+      );
+      thumbnailImage.addEventListener("click", () => {
+        // Reset brightness for all thumbnails
+        thumbnailContainer.querySelectorAll("img").forEach((img) => {
+          img.classList.remove("brightness-100");
+          img.classList.add("brightness-50");
+        });
+
+        // Handle the click event on thumbnail images
+        handleThumbnailClick(index, listing.media, thumbnailImage);
+
+        // Update the brightness class of the clicked thumbnail
+        thumbnailImage.classList.remove("brightness-50");
+        thumbnailImage.classList.add("brightness-100");
+      });
+      thumbnailContainer.appendChild(thumbnailImage);
+    });
+  }
 
   return gallerySection;
 }
-
-// export function imageGallery(listing) {
-//   const gallerySection = document.createElement("section");
-//   gallerySection.classList.add("flex", "flex-col", "gap-4"); // Create the first media item
-
-//   const firstMedia = document.createElement("img");
-//   firstMedia.classList.add("aspect-video", "object-cover", "rounded-md");
-//   firstMedia.src = listing.media[0];
-//   firstMedia.alt = listing.title;
-//   gallerySection.appendChild(firstMedia); // Create a container for the rest of the media items
-
-//   const nextMediaContainer = document.createElement("div");
-//   nextMediaContainer.classList.add(
-//     "py-2",
-//     "flex",
-//     "flex-wrap",
-//     "gap-1",
-//     "justify-center",
-//     "items-center"
-//   );
-//   gallerySection.appendChild(nextMediaContainer);
-
-//   listing.media.slice(1).forEach((mediaUrl) => {
-//     const nextMediaItem = document.createElement("img");
-//     nextMediaItem.classList.add(
-//       "h-10",
-//       "md:h-20",
-//       "aspect-square",
-//       "object-cover",
-//       "rounded-md",
-//       "brightness-50"
-//     );
-//     nextMediaItem.src = mediaUrl;
-//     nextMediaItem.alt = listing.title;
-//     nextMediaContainer.appendChild(nextMediaItem);
-//   });
-
-//   return gallerySection;
-// }
