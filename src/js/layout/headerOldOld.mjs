b5bf7372337f9d122943867bf5afc9b6cbb13472
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-04 16:57:33 +0100

    header fix

--- src/js/layout/headerOldOld.mjs
+++ src/js/layout/headerOldOld.mjs
@@ -4,7 +4,21 @@ import { checkLoggedIn } from "../auth/state.mjs";
 import { createButton } from "../utils/helper/createButton.mjs";
 import { createNavLink } from "../utils/helper/navLinks.mjs";
 
-function createLogo() {
+export function header() {
+  const isLoggedIn = checkLoggedIn();
+  const htmlHeader = document.createElement("header");
+  htmlHeader.classList.add("p-2", "shadow-lg", "bg-white");
+
+  const containerDiv = document.createElement("div");
+  containerDiv.classList.add(
+    "container",
+    "mx-auto",
+    "flex",
+    "items-center",
+    "justify-between",
+    "space-x-4"
+  );
+
   const logoLink = document.createElement("a");
   const logo = document.createElement("img");
   logo.classList.add("w-36");
@@ -12,31 +26,44 @@ function createLogo() {
   logo.setAttribute("alt", "Brand logo");
   logoLink.setAttribute("href", "/index.html");
   logoLink.appendChild(logo);
-  return logoLink;
-}
 
-function createNavigation() {
   const navigation = document.createElement("nav");
+  navigation.classList.add("flex", "flex-col", "sm:flex-row", "gap-2");
+
   const ul = document.createElement("ul");
   ul.classList.add("flex", "flex-col", "sm:flex-row", "gap-2");
-  navigation.classList.add("flex", "flex-col", "sm:flex-row", "gap-2");
 
   // Links
   const homeLink = createNavLink("Home", "/index.html");
+
   const listingsLink = createNavLink("Listings", "/src/html/listings.html");
+
   const profileLink = createNavLink("Profile", "#");
   profileLink.classList.add("nav-link-profile");
+  profileLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    const profile = loadToken("profile");
+    window.location = `/src/html/profile.html?name=${profile.name}`;
+  });
+
   const newList = createNavLink("Add", "#");
+  newList.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location = `/src/html/newListing.html`;
+  });
 
   ul.appendChild(homeLink);
   ul.appendChild(profileLink);
   ul.appendChild(listingsLink);
   ul.appendChild(newList);
-  navigation.appendChild(ul);
-  return navigation;
-}
 
-function createButtons() {
+  // Appends
+  containerDiv.appendChild(logoLink);
+  navigation.appendChild(ul);
+  containerDiv.appendChild(navigation);
+
+  // Buttons
+
   const buttonsDiv = document.createElement("div");
   buttonsDiv.classList.add(
     "flex",
@@ -62,28 +89,42 @@ function createButtons() {
 
   const profile = loadToken("profile");
   if (profile) {
-    const profileLink = createNavLink("Profile", "#");
-    profileLink.classList.add("nav-link-profile");
-    profileLink.addEventListener("click", (e) => {
-      e.preventDefault();
-      const profile = loadToken("profile");
-      window.location = `/src/html/profile.html?name=${profile.name}`;
-    });
-    return buttonsDiv;
+    const divElement = document.createElement("div");
+    divElement.classList.add("w-max", "rounded-2xl");
+    const divInner = document.createElement("div");
+    divInner.classList.add("flex", "gap-2", "align-middle", "p-2");
+    const profileImage = document.createElement("img");
+    profileImage.setAttribute("src", profile.avatar);
+    console.log("Avatar", profile.avatar);
+    profileImage.setAttribute("alt", "Profile image");
+    profileImage.classList.add(
+      "w-10",
+      "aspect-square",
+      "rounded-full",
+      "object-cover"
+    );
+    const newDiv = document.createElement("div");
+    const profileName = document.createElement("h3");
+    profileName.textContent = profile.name;
+    profileName.classList.add("text-black", "font-bold");
+    const profileCredits = document.createElement("p");
+    profileCredits.textContent = `Credits: ${profile.credits}`;
+    profileCredits.classList.add("text-black");
+    newDiv.appendChild(profileName);
+    newDiv.appendChild(profileCredits);
+    divInner.appendChild(profileImage);
+    divInner.appendChild(newDiv);
+    divElement.appendChild(divInner);
+    buttonsDiv.appendChild(divElement);
   }
+
   buttonsDiv.appendChild(loginBtn);
   buttonsDiv.appendChild(registerBtn);
   buttonsDiv.appendChild(logoutBtn);
-  return buttonsDiv;
-}
+  containerDiv.appendChild(buttonsDiv);
+  htmlHeader.appendChild(containerDiv);
 
-function hideElementBasedOnLoginStatus(
-  isLoggedIn,
-  loginBtn,
-  registerBtn,
-  logoutBtn,
-  profileLink
-) {
+  // Hide login and register buttons if logged in
   if (isLoggedIn) {
     loginBtn.classList.add("hidden");
     registerBtn.classList.add("hidden");
@@ -95,39 +136,6 @@ function hideElementBasedOnLoginStatus(
   if (!isLoggedIn) {
     profileLink.classList.add("hidden");
   }
-}
-
-export function header() {
-  const isLoggedIn = checkLoggedIn();
-  const htmlHeader = document.createElement("header");
-  htmlHeader.classList.add("p-2", "shadow-lg", "bg-white");
-
-  const containerDiv = document.createElement("div");
-  containerDiv.classList.add(
-    "container",
-    "mx-auto",
-    "flex",
-    "items-center",
-    "justify-between",
-    "space-x-4"
-  );
-
-  const logoLink = createLogo();
-  const navigation = createNavigation();
-  const buttonsDiv = createButtons();
-
-  containerDiv.appendChild(logoLink);
-  navigation.appendChild(buttonsDiv);
-  containerDiv.appendChild(navigation);
-  htmlHeader.appendChild(containerDiv);
-
-  hideElementBasedOnLoginStatus(
-    isLoggedIn,
-    buttonsDiv.children[0],
-    buttonsDiv.children[1],
-    buttonsDiv.children[2],
-    navigation.children[0].children[1]
-  );
 
   return htmlHeader;
 }
