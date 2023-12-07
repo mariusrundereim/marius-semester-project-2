3c014994e71a958eddf1d97b13a5adb18f045d35
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-07 16:52:18 +0100

    improvements

--- src/js/components/validation/newListingElements.mjs
+++
@@ -1,4 +0,0 @@
-export const titleError = document.querySelector("#titleError");
-export const descriptionError = document.querySelector("#descriptionError");
-export const mediaError = document.querySelector("#mediaError");
-export const endsAtError = document.querySelector("#endsAtError");
--- src/js/listeners/listings/createListing.mjs
+++ src/js/listeners/listings/createListing.mjs
@@ -1,40 +1,34 @@
 import { createListing } from "../../api/listings/createListing.mjs";
-import { createListingForm } from "../components/validation/newListingValidation.mjs"; // Add this import statement
 
-const form = document.querySelector("#newListing-form");
-createListingForm(form);
+export function createListingForm() {
+  const form = document.querySelector("#newListing-form");
 
-// import { createListing } from "../../api/listings/createListing.mjs";
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
 
-// export function createListingForm() {
-//   const form = document.querySelector("#newListing-form");
+    const mediaUrls = [];
+    // Iterate over media inputs
+    for (let i = 1; i <= 4; i++) {
+      const mediaInputValue = formData.get(`media-${i}`);
+      if (mediaInputValue) {
+        mediaUrls.push(mediaInputValue);
+      }
+    }
 
-//   form.addEventListener("submit", async (e) => {
-//     e.preventDefault();
-//     const formData = new FormData(form);
+    const newListing = {
+      title: formData.get("title"),
+      description: formData.get("description"),
+      media: mediaUrls,
+      endsAt: new Date(formData.get("endsAt")),
+    };
 
-//     const mediaUrls = [];
-//     // Iterate over media inputs
-//     for (let i = 1; i <= 4; i++) {
-//       const mediaInputValue = formData.get(`media-${i}`);
-//       if (mediaInputValue) {
-//         mediaUrls.push(mediaInputValue);
-//       }
-//     }
+    const createdListing = await createListing(newListing);
+    form.reset();
+    window.location.href = `./listingSpecific.html?id=${createdListing.id}`;
+  });
+}
 
-//     const newListing = {
-//       title: formData.get("title"),
-//       description: formData.get("description"),
-//       media: mediaUrls,
-//       endsAt: new Date(formData.get("endsAt")),
-//     };
-
-//     const createdListing = await createListing(newListing);
-//     form.reset();
-//     window.location.href = `./listingSpecific.html?id=${createdListing.id}`;
-//   });
-// }
-
-// (async () => {
-//   await createListingForm();
-// })();
+(async () => {
+  await createListingForm();
+})();
