bc7751b9f60c02b4d55343968dd4190304073bd6
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-10 20:26:06 +0100

    linking correctly

--- src/js/layout/header.mjs
+++ src/js/layout/header.mjs
@@ -66,16 +66,16 @@ function createNavigation() {
 
   if (isLoggedIn) {
     navItems.push(
-      { text: "Listings", href: "/listings.html" },
+      { text: "Listings", href: "./listings.html" },
       {
         text: "Profile",
         href: isLoggedIn ? `./profile.html?name=${profile.name}` : "#",
         id: "profile-link",
       },
-      { text: "Add", href: "/newListing.html" }
+      { text: "Add", href: "./newListing.html" }
     );
   } else {
-    navItems.push({ text: "Listings", href: "/listings.html" });
+    navItems.push({ text: "Listings", href: "./listings.html" });
   }
 
   navItems.forEach((item) => {
