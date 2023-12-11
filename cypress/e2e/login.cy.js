e19d7f9ba722ecc10bd40072de0ee8388bda0351
Marius Rundereim <marius.rundereim@gmail.com>
2023-12-11 16:17:37 +0100

    e2e testing login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,8 +5,14 @@ const wrongEmail = "mariusru@gmail.com";
 const wrongPass = "Test1234";
 
 describe("Wrong auth login", () => {
-  cy.visit("https://mariusrundereim.github.io/marius-semester-project-2/");
-  cy.clearLocalStorage();
-  cy.get("button").contains("Login").click();
-  cy.wait(1000);
+  it("Try your best", () => {
+    cy.visit("index.html");
+    cy.clearLocalStorage();
+    cy.get("button").contains("Login").click();
+    cy.wait(1000);
+    cy.get("input[name=email]").type(wrongEmail);
+    cy.get("input[name=password]").type(wrongPass);
+    cy.wait(600);
+    cy.get('[data-cy="login-submit"]').click();
+  });
 });
